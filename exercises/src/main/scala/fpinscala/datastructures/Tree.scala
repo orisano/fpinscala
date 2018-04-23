7ee311c62c0a3585105224edd3f9abe391a3f0e1
Nao YONASHIRO <owan.orisano@gmail.com>
2018-04-24 00:37:53 +0900

    feat: ex3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,19 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size[A](t: Tree[A]): Int = t match {
+    case Branch(l, r) => 1 + size(l) + size(r)
+    case Leaf(_) => 1
+  }
+}
 
+object TestSize {
 
+  import Tree.size
 
-
+  def main(args: Array[String]): Unit = {
+    assert(size(Branch(Leaf(1), Leaf(2))) == 3)
+    assert(size(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 5)
+    assert(size(Leaf(1)) == 1)
+  }
 }
