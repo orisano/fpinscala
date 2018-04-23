8c9b2c981d20aab62cf5a5052d28f4c7b00ae542
Nao YONASHIRO <owan.orisano@gmail.com>
2018-04-24 00:44:47 +0900

    feat: ex3.26

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -10,6 +10,11 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
     case Leaf(_) => 1
   }
+
+  def maximum(t: Tree[Int]): Int = t match {
+    case Branch(l, r) => maximum(l) max maximum(r)
+    case Leaf(v) => v
+  }
 }
 
 object TestSize {
@@ -20,5 +25,16 @@ object TestSize {
     assert(size(Branch(Leaf(1), Leaf(2))) == 3)
     assert(size(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 5)
     assert(size(Leaf(1)) == 1)
+  }
+}
+
+object TestMaximum {
+
+  import Tree.maximum
+
+  def main(args: Array[String]): Unit = {
+    assert(maximum(Leaf(100)) == 100)
+    assert(maximum(Branch(Leaf(100), Branch(Leaf(10), Leaf(10000)))) == 10000)
+    assert(maximum(Branch(Leaf(5), Leaf(10))) == 10)
   }
 }
