0e78a6e6b1fa63863cc334ab74ad115393fbd5fd
Nao YONASHIRO <owan.orisano@gmail.com>
2018-04-26 03:36:24 +0900

    feat: ex3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -25,6 +25,19 @@ object Tree {
     case Leaf(x) => Leaf(f(x))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = t match {
+    case Leaf(x) => l(x)
+    case Branch(x, y) => b(fold(x)(l)(b), fold(y)(l)(b))
+  }
+
+  def size2[A](t: Tree[A]): Int = fold(t)(_ => 1)((a, b) => a + b + 1)
+
+  def maximum2(t: Tree[Int]): Int = fold(t)(x => x)((a, b) => a.max(b))
+
+  def depth2[A](t: Tree[A]): Int = fold(t)(_ => 1)((a, b) => a.max(b) + 1)
+
+  def map2[A, B](t: Tree[A])(f: (A) => B): Tree[B] = fold[A, Tree[B]](t)(x => Leaf(f(x)))((a, b) => Branch(a, b))
 }
 
 object TestSize {
@@ -69,5 +82,32 @@ object TestMap {
     assert(map(Leaf(1))(x => x.toString) == Leaf("1"))
     assert(map(Branch(Leaf(1), Leaf(2)))(2.*) == Branch(Leaf(2), Leaf(4)))
     assert(map(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))))(4.*) == Branch(Leaf(4), Branch(Leaf(8), Leaf(12))))
+  }
+}
+
+object TestFold {
+
+  import Tree.size2
+  import Tree.maximum2
+  import Tree.depth2
+  import Tree.map2
+
+  def main(args: Array[String]): Unit = {
+    assert(size2(Branch(Leaf(1), Leaf(2))) == 3)
+    assert(size2(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 5)
+    assert(size2(Leaf(1)) == 1)
+
+    assert(maximum2(Leaf(100)) == 100)
+    assert(maximum2(Branch(Leaf(100), Branch(Leaf(10), Leaf(10000)))) == 10000)
+    assert(maximum2(Branch(Leaf(5), Leaf(10))) == 10)
+
+    assert(depth2(Leaf(1)) == 1)
+    assert(depth2(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 3)
+    assert(depth2(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) == 3)
+    assert(depth2(Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Branch(Leaf(4), Leaf(5)))))) == 5)
+
+    assert(map2(Leaf(1))(x => x.toString) == Leaf("1"))
+    assert(map2(Branch(Leaf(1), Leaf(2)))(2.*) == Branch(Leaf(2), Leaf(4)))
+    assert(map2(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))))(4.*) == Branch(Leaf(4), Branch(Leaf(8), Leaf(12))))
   }
 }
