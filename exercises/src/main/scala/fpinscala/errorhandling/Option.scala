40d2988a21a592ed4bf58aa54615f08e9ef2ebf9
Nao YONASHIRO <owan.orisano@gmail.com>
2018-04-27 01:46:21 +0900

    feat: ex4.1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,18 +4,48 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = map(x => Some(x)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = flatMap(x => if (f(x)) Some(x) else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
+
+object OptionTest {
+
+  def main(args: Array[String]): Unit = {
+    assert(Some(2).map(4.*) == Some(8))
+    assert((None: Option[Int]).map(4.*) == None)
+
+    assert(Some("hello").getOrElse("foo") == "hello")
+    assert((None: Option[String]).getOrElse("foo") == "foo")
+
+    assert(Some(5).flatMap(x => Some(x.toString)) == Some("5"))
+    assert((None: Option[Int]).flatMap(x => Some(x.toString)) == None)
+
+    assert(Some(5).orElse(Some(10)) == Some(5))
+    assert((None: Option[Int]).orElse(Some(10)) == Some(10))
+
+    assert(Some(6).filter(x => x % 2 == 0) == Some(6))
+    assert(Some(5).filter(x => x % 2 == 0) == None)
+    assert((None: Option[Int]).filter(x => x % 2 == 0) == None)
+  }
+}
+
 
 object Option {
   def failingFn(i: Int): Int = {
@@ -24,7 +54,9 @@ object Option {
       val x = 42 + 5
       x + y
     }
-    catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch {
+      case e: Exception => 43
+    } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
@@ -32,15 +64,18 @@ object Option {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
   def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
